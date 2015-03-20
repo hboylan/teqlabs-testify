83c0030fcd2018e7b0b4d2228952bd98a288c70b
Hugh Boylan <hugh@Hughs-MacBook-Pro.local>
2015-03-20 05:35:01 -0400

    bearer token, updates

--- README.md
+++ README.md
@@ -10,13 +10,25 @@ var api = new Test('localhost', 8000)
 
 ## Functions
 
- Method        | Action 
+ Method        | Action
 -------------  | -----------------------------------------------------------------------------------------------------------
-`get()`        | [GET Request](https://bitbucket.org/hboylan/nomadicfitness-api#markdown-header-get-request)
+`request()`    | [Request Wrapper](#request-wrapper)
+`get()`        | [GET Request](#get-request)
 `post()`       | [POST Request](https://bitbucket.org/hboylan/nomadicfitness-api#markdown-header-post-request)
 `put()`        | [PUT Request](https://bitbucket.org/hboylan/nomadicfitness-api#markdown-header-put-request)
 `del()`        | [DELETE Request](https://bitbucket.org/hboylan/nomadicfitness-api#markdown-header-delete-request)
 `cookie(name)` | [Set Cookie](https://bitbucket.org/hboylan/nomadicfitness-api#markdown-header-set-cookie)
+
+- - -
+
+# Request Wrapper
+```javascript
+api.get('/users', function (res, users) {
+  assert.equal(res.statusCode, 200)
+  assert.equal(users[0].role, 'client')
+  // etc...
+})
+```
 
 - - -
 
@@ -114,7 +126,7 @@ RESPONSE: [200]
 ```javascript
 api.post('/login', { email:'bluehugh2@gmail.com', password:'password1' }, function (res, user) {
   api.cookie('connect.sid', res)
-  
+
   api.get('/auth', function (res, user) { // with expressjs, check req.session is valid
     assert.equal(res.statusCode, 200)
     assert.equal(user.email, 'bluehugh2@gmail.com')
--- index.js
+++ index.js
@@ -1,57 +1,63 @@
 var request = require('request')
 
-function Test (host, port, secure) {
-  this._rootAPI = secure? 'https://' : 'http://'
-  this._rootAPI += host + ':' + (port || 80)
+function Test (opts) {
+  this._params = {}
+  this._rootAPI = 'http'
+  if(opts.secure){
+    this._params.
+    this._rootAPI += 's'
+  }
+  this._rootAPI += '://' + opts.host + ':' + (opts.port || 80)
   this._headers = {
     'Content-Type': 'application/json'
   }
-  console.log('Testing '+this._rootAPI)
+  console.log('Testing ' + this._rootAPI)
   console.log()
 }
 module.exports = Test
 
-Test.prototype._req = function (method, path, data, next) {
-  if(typeof data == 'function') {
-    next = data
-    data = {}
-  }
-  console.info(method.toUpperCase(), path)
-  console.info(JSON.stringify(data, null, 2))
-  request({
-    method: method,
-    uri: this._rootAPI + path,
-    json: data,
-    headers: this._headers
-  }, function (err, res, body) {
+Test.prototype._req = function (params, next) {
+  console.info(method.toUpperCase(), params.uri)
+  console.info(JSON.stringify(params.json, null, 2))
+  request(params, function (err, res, body) {
     if(err) console.error(err)
-    console.log('RESPONSE:', '['+res.statusCode+']')
+    console.log('RESPONSE:', '[' + res.statusCode + ']')
     console.log(JSON.stringify(body, null, 2) || null)
     console.log()
     next(res, body)
   })
 }
 
+Test.prototype.request = function (opts, next) {
+  opts.headers = this._headers
+  opts.uri = this._rootAPI + opts.path
+  this._req(opts, next)
+}
+
 Test.prototype.get = function (path, next) {
-  this._req('GET', path, next)
+  this.request({ path:path }, next)
 }
 
 Test.prototype.post = function (path, data, next) {
-  this._req('POST', path, data, next)
+  this.request({ method:'POST', path:path, json:data }, next)
 }
 
 Test.prototype.put = function (path, data, next) {
-  this._req('PUT', path, data, next)
+  this.request({ method:'PUT', path:path, json:data }, next)
 }
 
 Test.prototype.del = function (path, data, next) {
-  this._req('DELETE', path, data, next)
+  this.request({ method:'DELETE', path:path, json:data }, next)
 }
 
 Test.prototype.cookie = function (name, res) {
-  var headers = this._headers
-  res.headers['set-cookie'].filter(function(cookie) {
-    if (!!~cookie.indexOf(name))
-      headers['Cookie'] = cookie
-  })
+  this._headers['Cookie'] = cookie
+}
+
+Test.prototype.bearer = function (token) {
+  this._headers['Authorization'] = 'Bearer ' + token
+}
+
+Test.prototype.setHeader = function (key, val) {
+  this._headers[key] = val
 }
